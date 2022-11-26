021d9b0330436d5693beafd4cdc27a45e3cd1b67
Freddy Sebastian Garcia <fsega08@gmail.com>
2022-11-26 10:44:35 -0500

    Se remueve configuracion de cors innecesaria

--- bank-customer/src/main/java/co/com/coomeva/bank/BankCustomerApplication.java
+++ bank-customer/src/main/java/co/com/coomeva/bank/BankCustomerApplication.java
@@ -1,13 +1,7 @@
 package co.com.coomeva.bank;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class BankCustomerApplication {
@@ -16,15 +10,4 @@ public class BankCustomerApplication {
 		SpringApplication.run(BankCustomerApplication.class, args);
 	}
 	
-	
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS", "HEAD"));
-		configuration.addAllowedHeader("*");
-		configuration.addAllowedOrigin("*");
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
 }
